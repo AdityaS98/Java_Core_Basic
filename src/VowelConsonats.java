9f32f727eb8eaa893f70ec7970525531fe2330bd
AdityaS98 <adityasharmalko1998@gmail.com>
2023-07-19 12:05:15 +0530

    Adding a program for finding vowels or consonats

---
+++ src/VowelConsonats.java
@@ -0,0 +1,29 @@
+package com.examples;
+
+import java.util.Scanner;
+
+public class VowelConsonats {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter alphabet");
+
+        char ch = sc.next().charAt(0);
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+
+
+    }
+}
+
+
